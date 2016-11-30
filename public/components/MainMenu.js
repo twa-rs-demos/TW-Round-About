8ad2526d341187aa96969d05a1b13a31d3089d8f
seafruit <18829290140@163.com>
2016-11-30 17:03:48 +0800

    separate index page top column

---
+++ public/components/MainMenu.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+
+export default  class MainMenu extends Component {
+  render() {
+    return (
+      <div>
+        <div className="top-right-items row">
+          <div className="col-md-8"></div>
+          <div className="items col-md-4 row">
+            <a href="#" className="col-sm-3">月报中心</a>
+            <a href="#" className="col-sm-3">月报申领</a>
+            <a href="#" className="col-sm-2">English</a>
+            <a href="#">
+              <i className="fa fa-weixin fa-2x col-sm-1" aria-hidden="true"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-weibo fa-2x col-sm-1" aria-hidden="true"></i>
+            </a>
+            <a href="#">
+              {/*<img src="../images/taobao-top.png" alt="" className="taobao">*/}
+            </a>
+          </div>
+        </div>
+      </div>
+
+    )
+  }
+
+}
