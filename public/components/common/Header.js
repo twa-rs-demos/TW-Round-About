3a3700a9c5c878d182e846f602ba2ec3db9e248f
XLuoChen <1425145741@qq.com>
2016-12-09 23:16:32 +0800

    add SearchBox component

--- public/components/common/Header.js
+++ public/components/common/Header.js
@@ -2,14 +2,18 @@ import React, {Component} from 'react';
 import RightTopinfo from "../common/RightTopinfo";
 import MenuList from "../common/MenuList";
 import Logo from '../common/Logo';
+import SearchBox from './SearchBox';
 
-export default class Header extends Component{
-  render(){
+export default class Header extends Component {
+  render() {
     return (
       <div className="header row">
         <RightTopinfo/>
         <Logo/>
-        <MenuList/>
+        <div className="col-sm-12 no-padding ">
+          <MenuList/>
+          <SearchBox/>
+        </div>
       </div>
     )
   }
