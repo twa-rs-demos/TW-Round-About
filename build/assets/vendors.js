d31e65ed2c6c4252649ecd90c401bbd1be5d2b51
seafruit <18829290140@163.com>
2016-12-12 11:26:22 +0800

    modified Footer and rightTopInfo

--- build/assets/vendors.js
+++ build/assets/vendors.js
@@ -76,7 +76,7 @@
 /******/ 			script.charset = 'utf-8';
 /******/ 			script.async = true;
 
-/******/ 			script.src = __webpack_require__.p + "" + ({"0":"entry"}[chunkId]||chunkId) + "." + {"0":"c3e43935"}[chunkId] + ".js";
+/******/ 			script.src = __webpack_require__.p + "" + ({"0":"entry"}[chunkId]||chunkId) + "." + {"0":"e1b49075"}[chunkId] + ".js";
 /******/ 			head.appendChild(script);
 /******/ 		}
 /******/ 	};
@@ -40457,7 +40457,62 @@
 /* 513 */,
 /* 514 */,
 /* 515 */,
-/* 516 */,
+/* 516 */
+/***/ function(module, exports) {
+
+	/*
+		MIT License http://www.opensource.org/licenses/mit-license.php
+		Author Tobias Koppers @sokra
+	*/
+	// css base code, injected by the css-loader
+	module.exports = function() {
+		var list = [];
+
+		// return the list of modules as css string
+		list.toString = function toString() {
+			var result = [];
+			for(var i = 0; i < this.length; i++) {
+				var item = this[i];
+				if(item[2]) {
+					result.push("@media " + item[2] + "{" + item[1] + "}");
+				} else {
+					result.push(item[1]);
+				}
+			}
+			return result.join("");
+		};
+
+		// import a list of modules into the list
+		list.i = function(modules, mediaQuery) {
+			if(typeof modules === "string")
+				modules = [[null, modules, ""]];
+			var alreadyImportedModules = {};
+			for(var i = 0; i < this.length; i++) {
+				var id = this[i][0];
+				if(typeof id === "number")
+					alreadyImportedModules[id] = true;
+			}
+			for(i = 0; i < modules.length; i++) {
+				var item = modules[i];
+				// skip already imported module
+				// this implementation is not 100% perfect for weird media query combinations
+				//  when a module is imported multiple times with different media queries.
+				//  I hope this will never occur (Hey this way we have smaller bundles)
+				if(typeof item[0] !== "number" || !alreadyImportedModules[item[0]]) {
+					if(mediaQuery && !item[2]) {
+						item[2] = mediaQuery;
+					} else if(mediaQuery) {
+						item[2] = "(" + item[2] + ") and (" + mediaQuery + ")";
+					}
+					list.push(item);
+				}
+			}
+		};
+		return list;
+	};
+
+
+/***/ },
 /* 517 */,
 /* 518 */,
 /* 519 */,
--- public/components/common/RightTopinfo.js
+++ public/components/common/RightTopinfo.js
@@ -3,21 +3,21 @@ import React, {Component} from 'react';
 export default  class MainMenu extends Component {
   render() {
     return (
-        <div className="items col-md-4 row top-right-items">
-          <a href="#" className="col-sm-3">月报中心</a>
-          <a href="#" className="col-sm-3">月报申领</a>
-          <a href="#" className="col-sm-2">English</a>
-          <a href="#" className="col-sm-1">
-            <img src="./public/images/icon_wechat.png" className="taobao"/>
-          </a>
-          <a href="#" className="col-sm-1">
-            <img src="./public/images/icon_weibo.png" className="taobao"/>
-          </a>
-          <a href="#" className="col-sm-1">
-            <img src="./public/images/icon_taobao.png" alt="" className="taobao"/>
-          </a>
-        </div>
+      <div className="items col-md-3 row top-right-items no-padding">
+
+        <a href="#" className="col-sm-2 no-padding">月报中心</a>
+        <a href="#" className="col-sm-2 no-padding">月报申领</a>
+        <a href="#" className="col-sm-2 no-padding">English</a>
+        <a href="#" className="col-sm-1 no-padding">
+          <img src="./public/images/icon_wechat.png" className="taobao"/>
+        </a>
+        <a href="#" className="col-sm-1 no-padding">
+          <img src="./public/images/icon_weibo.png" className="taobao"/>
+        </a>
+        <a href="#" className="col-sm-1 no-padding">
+          <img src="./public/images/icon_taobao.png" alt="" className="taobao"/>
+        </a>
+      </div>
     );
   }
-
 }
