4f5d2b42b3590ee8c8980462ef3313c31e0a8fce
seafruit <18829290140@163.com>
2016-12-12 18:05:35 +0800

    :art: delete build

--- public/components/layOut/MenuList.js
+++ public/components/layOut/MenuList.js
@@ -1,8 +1,26 @@
 import React, {Component} from 'react';
 import {NavDropdown, MenuItem} from 'react-bootstrap';
+import request from 'superagent';
+import noCache from 'superagent-no-cache';
 
 
 export default class MenuList extends Component {
+  // constructor(){
+  //   this.state = {
+  //     menuList:[]
+  //   }
+  // }
+  componentDidMount() {
+    request.get('http://127.0.0.1/wp-json/wp/v2/pages/34')
+      .end((err, res)=> {
+        if (err) {
+          alert(err);
+        }
+        console.log(res.body);
+        // this.state.menuList = res;
+      });
+  }
+
   getMenuItemList(item) {
     return item.map((menuItem)=> {
       return <MenuItem value="捐赠" className="menu-item">{menuItem}</MenuItem>
