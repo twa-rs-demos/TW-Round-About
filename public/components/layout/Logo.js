e588578593e062ddaee5784c600f20ac0c2e889b
MHwishes <1025383143@qq.com>
2016-12-13 23:01:23 +0800

    add logon router

--- public/components/layout/Logo.js
+++ public/components/layout/Logo.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router';
 
 export default  class Logo extends Component {
+
   render() {
     return (
-        <div className="row logo">
-          <img src="./public/images/home/index_logo1.png" alt="logo"/>
-        </div>
+      <div className="row logo">
+        <Link to="/tw-ra/"> <img src="./public/images/home/index_logo1.png" alt="logo"/></Link>
+      </div>
     )
   }
-
 }
