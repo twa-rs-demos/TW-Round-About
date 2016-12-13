502f40bb55ae263b52285121b7c4265b8fa26431
seafruit <18829290140@163.com>
2016-12-13 20:07:40 +0800

    add video

--- public/components/home/Video.js
+++ public/components/home/Video.js
@@ -17,7 +17,6 @@ export default class media extends Component {
         if (err) {
           throw(err);
         } else {
-          console.log((res.body.content.rendered));
           this.setState({
             content: res.body.content.rendered
           });
@@ -27,18 +26,7 @@ export default class media extends Component {
 
   render() {
     return (
-      <div id="test">
-        {xmlDoc}
-      </div>
+      <div className="content" dangerouslySetInnerHTML={{__html: this.state.content}}></div>
     )
   }
 }
-
-/*<div className="middle-text">*/
-/*<p> 我们是众爱</p>*/
-/*<span> 众爱是连接捐款人与受助人的桥梁</span>*/
-/*// </div>*/
-/*// <div className="show-video">*/
-/*//   <embed src="http://www.iqiyi" width="620" height="400" className="video" autostart="true/false"*/
-/*//          loop="true/false"></embed>*/
-/*// </div>*/
