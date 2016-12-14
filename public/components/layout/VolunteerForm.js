4d1f6c1f8ce56d8a76a9d011b7fff7c1a64542ac
MHwishes <1025383143@qq.com>
2016-12-14 18:27:13 +0800

    add volunteer style

--- public/components/layout/VolunteerForm.js
+++ public/components/layout/VolunteerForm.js
@@ -1,12 +1,18 @@
 import {Component} from 'react';
 import {Modal, Button} from 'react-bootstrap';
+import '../../style/modal.css';
 
 export default class VolunteerForm extends Component {
 
-  cancelButton(){
+  cancelButton() {
     this.props.showVolunteerForm();
   }
-  confirmButton(){
+
+  confirmButton() {
+    this.props.showVolunteerForm();
+  }
+
+  close(){
     this.props.showVolunteerForm();
   }
 
@@ -14,25 +20,67 @@ export default class VolunteerForm extends Component {
     return (
       <div>
 
-          <div className='static-modal'>
-            <Modal.Dialog>
-              <Modal.Header>
-                <Modal.Title>删除提示</Modal.Title>
-              </Modal.Header>
+        <div className='static-modal'>
+          <Modal.Dialog>
+            <Modal.Header>
+              <Modal.Title>
+                <div className="row">
+                  <div className="col-xs-5 col-xs-offset-1">
+                    VOLUNTEERS APPLICATION FORM
+                  </div>
+                  <div className="col-xs-offset-5 col-xs-1">
+                    <img src="./public/images/home/close.png" onClick={this.close.bind(this)}/>
+                  </div>
+                </div>
+              </Modal.Title>
+            </Modal.Header>
 
-              <Modal.Body>
-                您确定要删除吗？
-              </Modal.Body>
+            <Modal.Body>
+              <div className="row info">
+                <div className=" col-xs-2 info-right">
+                  <span> FistName<span className="pink">*</span></span>
+                </div>
+                <div className="col-xs-3">
+                  <input type="text"/>
+                </div>
 
-              <Modal.Footer>
-                <Button onClick={this.cancelButton.bind(this)}>取消</Button>
-                <Button bsStyle='primary' onClick={this.confirmButton.bind(this)}>确定</Button>
-              </Modal.Footer>
+                <div className=" col-xs-3 info-right">
+                  <span> LastName<span className="pink">*</span></span>
+                </div>
+                <div className="col-xs-3">
+                  <input type="text"/>
+                </div>
+              </div>
 
-            </Modal.Dialog>
-          </div>
+
+              <div className="row info">
+                <div className=" col-xs-2 info-right">
+                  <span> Age<span className="pink">*</span></span>
+                </div>
+                <div className="col-xs-3">
+                  <input type="text"/>
+                </div>
+
+                <div className=" col-xs-3 info-right">
+                  <span>TelephoneNumber<span className="pink">*</span></span>
+                </div>
+                <div className="col-xs-3">
+                  <input type="text"/>
+                </div>
+              </div>
+
+
+            </Modal.Body>
+
+            <Modal.Footer>
+              <Button onClick={this.cancelButton.bind(this)}>取消</Button>
+              <Button bsStyle='primary' onClick={this.confirmButton.bind(this)}>确定</Button>
+            </Modal.Footer>
+
+          </Modal.Dialog>
         </div>
-     
+      </div>
+
     );
   }
 }
