9da107e31924d5a5a1107aa7a112fbd4f47e3b12
XLuoChen <1425145741@qq.com>
2016-12-14 13:57:49 +0800

    modify menu style use jquery

--- public/components/layout/NewNavDropdown.js
+++ public/components/layout/NewNavDropdown.js
@@ -1,5 +1,5 @@
 import {Component} from 'react';
-import {NavDropdown,MenuItem} from 'react-bootstrap';
+import {NavDropdown, MenuItem} from 'react-bootstrap';
 import superagent from 'superagent';
 import noCache from  'superagent-no-cache';
 
@@ -12,32 +12,40 @@ export default class NewNavDropdown extends Component {
   }
 
   getMenuItem() {
-     return this.state.menuItemList.map((menuItem, index)=> {
+    return this.state.menuItemList.map((menuItem, index) => {
       return <MenuItem value={menuItem.name} id={index} className="menu-item" key={index}>{menuItem.name}</MenuItem>
-     });
+    });
   }
 
   componentDidMount() {
     superagent
       .get(`/wp-json/wp/v2/categories?parent=${this.props.id}`)
       .use(noCache)
-      .end((err, res)=> {
+      .end((err, res) => {
         if (err) {
           throw (err);
         } else {
           this.setState({
-            menuItemList:res.body
+            menuItemList: res.body
           })
         }
       });
   }
 
+  setMenuStyle() {
+    const currentElement = $(`#${this.props.id}`);
+    currentElement.css({'border-radius': '100px', 'background-color': '#EB4B99', 'color': '#FFFFFF'});
+    const menus = currentElement.parent().siblings();
+    Array.from(menus).forEach(menu => {
+      menu.firstChild.removeAttribute("style");
+    });
+  }
+
   render() {
     return (
-      <NavDropdown title={this.props.title} id={this.props.id} eventKey="1">
+      <NavDropdown title={this.props.title} id={this.props.id} eventKey="1" onClick={this.setMenuStyle.bind(this)}>
         {this.getMenuItem()}
       </NavDropdown>
-
     );
   }
 }
