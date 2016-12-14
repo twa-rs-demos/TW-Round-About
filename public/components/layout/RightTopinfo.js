168671e9e2f88ea6c7ce24d0ae944d8da9d51bf4
XLuoChen <1425145741@qq.com>
2016-12-14 20:30:12 +0800

    add link for right icon

--- public/components/layout/RightTopinfo.js
+++ public/components/layout/RightTopinfo.js
@@ -1,21 +1,57 @@
 import React, {Component} from 'react';
+import {Modal,Button} from 'react-bootstrap';
+
+class WechatModal extends Component {
+  render() {
+    return (
+      <div className="static-modal">
+        <Modal.Dialog>
+          <Modal.Header>
+            <Modal.Title>Modal title</Modal.Title>
+            <Button>X</Button>
+          </Modal.Header>
+
+          <Modal.Body>
+            One fine body...
+          </Modal.Body>
+
+        </Modal.Dialog>
+      </div>
+    )
+  }
+}
 
 export default  class MainMenu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShowWechat: false
+    };
+  }
+
+  showWechat() {
+    this.setState({isShowWechat: true});
+  }
+
   render() {
     return (
       <div className="items col-md-3 row top-right-items no-padding">
         <a href="#" className="col-sm-2 no-padding">月报中心</a>
         <a href="#" className="col-sm-2 no-padding">月报申领</a>
         <a href="#" className="col-sm-2 no-padding">English</a>
-        <a href="#" className="col-sm-1 no-padding">
-          <img src="./public/images/icon_wechat.png" className="taobao"/>
+        <a className="col-sm-1 no-padding" onClick={this.showWechat.bind(this)}>
+          <img src="./public/images/icon_wechat.png"/>
         </a>
-        <a href="#" className="col-sm-1 no-padding">
-          <img src="./public/images/icon_weibo.png" className="taobao"/>
+        <a href="http://weibo.com/Rooundabout666?sudaref=www.so.com&is_all=1"
+           className="col-sm-1 no-padding" target="_blank">
+          <img src="./public/images/icon_weibo.png"/>
         </a>
-        <a href="#" className="col-sm-1 no-padding">
-          <img src="./public/images/icon_taobao.png" alt="" className="taobao"/>
+        <a href="https://shop125100050.taobao.com/index.htm?spm=2013.1.w5002-12973215066.2.AXTtj4"
+           className="col-sm-1 no-padding" target="_blank">
+          <img src="./public/images/icon_taobao.png" alt=""/>
         </a>
+        {this.state.isShowWechat ? <WechatModal/> : ''}
+        {/*<WechatModal/>*/}
       </div>
     );
   }
