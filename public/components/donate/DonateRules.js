ae43d48c66f42ef3e5b1dfda5c490a4e70c0fc7f
seafruit <18829290140@163.com>
2016-12-16 12:09:06 +0800

    add donation apply from in donate page

---
+++ public/components/donate/DonateRules.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {Modal, Button} from 'react-bootstrap';
+
+export default class DonateRules extends Component {
+  render() {
+    return (
+      <div className="donate-things-rules">
+        <Modal.Dialog>
+          <Modal.Header>
+            <Button onClick={this.props.closeElasticBox}><img className="close-btn"
+                                                              src="./public/images/home/close.png"/></Button>
+            <h2>我们不接受的物品</h2>
+          </Modal.Header>
+
+          <Modal.Body>
+            <ul>
+              <li>Open, prescription and out of date medicines.</li>
+              <li>Used underwear, including socks.</li>
+              <li>Anything previously used by an animal – such as a dog’s bed</li>
+              <li>Copies of magazines over 6 months old.</li>
+              <li>Computers, monitors and printers over 4 years old.</li>
+            </ul>
+          </Modal.Body>
+        </Modal.Dialog>
+      </div>
+    )
+  }
+}
