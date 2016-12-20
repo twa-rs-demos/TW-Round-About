18fdf244f8f62b0e6d28124fc14935a3c52267b5
MHwishes <1025383143@qq.com>
2016-12-20 20:05:16 +0800

    refactor header

---
+++ public/components/layout/header/Header.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import NavMenu from "./NavMenu";
+import TopInfo from './TopInfo';
+import '../../../style/header.css';
+
+export default class Header extends Component {
+  render() {
+    return (
+
+      <div id="header">
+        <TopInfo/>
+        <NavMenu/>
+      </div>
+
+    )
+  }
+}
