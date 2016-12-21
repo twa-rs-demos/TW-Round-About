1d13df0ee6bc365f4d3c71a423e24a1ed813d5a5
MHwishes <1025383143@qq.com>
2016-12-21 14:05:09 +0800

    format code & modified privacy Policy style

--- public/components/legal/index.js
+++ public/components/legal/index.js
@@ -5,8 +5,9 @@ export default class Legal extends Component {
   render() {
     return (
       <div className="Legal">
-        <h2 className="title">Legal</h2>
-        <p className="Legal-content">Welcome to our website. If you continue to browse and use this website you are agreeing to comply with and be
+        <h2 className="Legal-title">Legal</h2>
+        <p className="Legal-content">Welcome to our website. If you continue to browse and use this website you are
+          agreeing to comply with and be
           bound by the following terms and conditions of use, which together with our privacy policy govern Roundabout’s
           relationship with you in relation to this website.</p>
         <p className="Legal-content">The term “Roundabout” or “us” or “we” refers to the owner of the website.</p>
@@ -14,35 +15,45 @@ export default class Legal extends Component {
 
         <p>The use of this website is subject to the following terms of use:</p>
         <ul>
-          <li className="li-item">The content of the pages of this website is for your general information and use only. It is subject to
+          <li className="li-item">The content of the pages of this website is for your general information and use only.
+            It is subject to
             change without notice.
           </li>
-          <li className="li-item">Neither we nor any third parties provide any warranty or guarantee as to the accuracy, timeliness,
+          <li className="li-item">Neither we nor any third parties provide any warranty or guarantee as to the accuracy,
+            timeliness,
             performance, completeness or suitability of the information and materials found or offered on this website
             for any particular purpose. You acknowledge that such information and materials may contain inaccuracies or
             errors and we expressly exclude liability for any such inaccuracies or errors to the fullest extent
             permitted by law.
           </li>
-          <li className="li-item">Your use of any information or materials on this website is entirely at your own risk, for which we shall
+          <li className="li-item">Your use of any information or materials on this website is entirely at your own risk,
+            for which we shall
             not be liable. It shall be your own responsibility to ensure that any products, services or information
             available through this website meet your specific requirements.
           </li>
-          <li className="li-item">This website contains material which is owned by or licensed to us. This material includes, but is not
+          <li className="li-item">This website contains material which is owned by or licensed to us. This material
+            includes, but is not
             limited to, the design, layout, look, appearance and graphics. Reproduction is prohibited other than in
             accordance with the copyright notice, which forms part of these terms and conditions.
           </li>
-          <li className="li-item">All trade marks reproduced in this website which are not the property of, or licensed to, the operator are
+          <li className="li-item">All trade marks reproduced in this website which are not the property of, or licensed
+            to, the operator are
             acknowledged on the website.
           </li>
-          <li className="li-item">Unauthorised use of this website may give rise to a claim for damages and/or be a criminal offence.</li>
-          <li className="li-item">From time to time this website may also include links to other websites. These links are provided for your
+          <li className="li-item">Unauthorised use of this website may give rise to a claim for damages and/or be a
+            criminal offence.
+          </li>
+          <li className="li-item">From time to time this website may also include links to other websites. These links
+            are provided for your
             convenience to provide further information. They do not signify that we endorse the website(s). We have no
             responsibility for the content of the linked website(s).
           </li>
-          <li className="li-item">You may not create a link to this website from another website or document without Roundabout’s prior
+          <li className="li-item">You may not create a link to this website from another website or document without
+            Roundabout’s prior
             written consent.
           </li>
-          <li className="li-item">Your use of this website and any dispute arising out of such use of the website is subject to the laws of
+          <li className="li-item">Your use of this website and any dispute arising out of such use of the website is
+            subject to the laws of
             The People’s Republic of China.
           </li>
         </ul>
@@ -51,11 +62,15 @@ export default class Legal extends Component {
         <p>Any redistribution or reproduction of part or all of the contents in any form is prohibited other than the
           following:</p>
         <ul>
-          <li className="li-item">you may print or download to a local hard disk extracts for your personal and non-commercial use only</li>
-          <li className="li-item">you may copy the content to individual third parties for their personal use, but only if you acknowledge
+          <li className="li-item">you may print or download to a local hard disk extracts for your personal and
+            non-commercial use only
+          </li>
+          <li className="li-item">you may copy the content to individual third parties for their personal use, but only
+            if you acknowledge
             the website as the source of the material
           </li>
-          <li className="li-item">You may not, except with our express written permission, distribute or commercially exploit the content.
+          <li className="li-item">You may not, except with our express written permission, distribute or commercially
+            exploit the content.
             Nor may you transmit it or store it in any other website or other form of electronic retrieval system.
           </li>
         </ul>
--- public/components/privacyPolicy/index.js
+++ public/components/privacyPolicy/index.js
@@ -5,7 +5,7 @@ export default class PrivacyPolicy extends Component {
   render() {
     return (
       <div className="Legal">
-        <h2 className="title">Privacy Policy</h2>
+        <h2 className="Legal-title">Privacy Policy</h2>
         <p className="Legal-content">This privacy policy sets out how “Roundabout” uses and protects any information
           that you give“Roundabout”when
           you use this website.</p>
@@ -51,17 +51,20 @@ export default class PrivacyPolicy extends Component {
 
         <div className="item-sections">
           <p className="item-title">How we use cookies</p>
-          <p className="Legal-content">A cookie is a small file which asks permission to be placed on your computer’s hard drive. Once you agree,
+          <p className="Legal-content">A cookie is a small file which asks permission to be placed on your computer’s
+            hard drive. Once you agree,
             the file is added and the cookie helps analyse web traffic or lets you know when you visit a particular
             site.
             Cookies allow web applications to respond to you as an individual. The web application can tailor its
             operations to your needs, likes and dislikes by gathering and remembering information about your
             preferences.</p>
 
-          <p className="Legal-content">We use traffic log cookies to identify which pages are being used. This helps us analyse data about webpage
+          <p className="Legal-content">We use traffic log cookies to identify which pages are being used. This helps us
+            analyse data about webpage
             traffic and improve our website in order to tailor it to customer needs. We only use this information for
             statistical analysis purposes and then the data is removed from the system.</p>
-          <p className="Legal-content">Overall, cookies help us provide you with a better website, by enabling us to monitor which pages you find
+          <p className="Legal-content">Overall, cookies help us provide you with a better website, by enabling us to
+            monitor which pages you find
             useful and which you do not. A cookie in no way gives us access to your computer or any information about
             you,
             other than the data you choose to share with us.</p>
@@ -71,19 +74,22 @@ export default class PrivacyPolicy extends Component {
         </div>
         <div className="item-sections">
           <p className="item-title">Links to other websites</p>
-          <p className="Legal-content">Our website may contain links to other websites of interest. However, once you have used these links to
+          <p className="Legal-content">Our website may contain links to other websites of interest. However, once you
+            have used these links to
             leave
             our site, you should note that we do not have any control over that other website. Therefore, we cannot be
             responsible for the protection and privacy of any information which you provide whilst visiting such sites
             and
             such sites are not governed by this privacy statement. You should exercise caution and look at the privacy
             statement applicable to the website in question.</p>
-          <p className="Legal-content">We will not sell, distribute or lease your personal information to third parties unless we have your
+          <p className="Legal-content">We will not sell, distribute or lease your personal information to third parties
+            unless we have your
             permission or are required by law to do so. We may use your personal information to send you information
             about
             third parties which we think you may find interesting if you tell us that you wish this to happen.</p>
 
-          <p className="Legal-content">You may request details of personal information which we hold about you under the Data Protection Act 1998.
+          <p className="Legal-content">You may request details of personal information which we hold about you under the
+            Data Protection Act 1998.
             A
             small fee will be payable. If you would like a copy of the information held on you please write to“
             Roundabout, Adjacent to Yosemite Villa Compound, Yuyang Road West, Off An Hua Road, SHUNYI, Beijing 101302.
