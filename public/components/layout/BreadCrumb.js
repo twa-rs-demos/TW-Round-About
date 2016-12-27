12f077f4b60cf176dcadb180c038a6a5e3d30b2f
MHwishes <1025383143@qq.com>
2016-12-27 14:25:21 +0800

    add breadcrumb &style

--- public/components/layout/BreadCrumb.js
+++ public/components/layout/BreadCrumb.js
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
+import '../../style/breadcrumb.less';
 
 export default class BreadCrumb extends Component {
 
   render() {
     const breadcrumb = this.props.breadCrumb;
+    let list = '';
+    if (breadcrumb.length != 0) {
+      list = breadcrumb.map((item, index)=> {
+          return <li key={index}>{item.text}</li>
+        }
+      )
+    }
+    const isHidden = list === '' ? 'hidden' : '';
     return (
-      <div >
-        {breadcrumb.length === 0 ? '' : `${breadcrumb[0].text}>${breadcrumb[1].text}`}
-      </div>);
+      <ol id='breadcrumb' className={'breadcrumb ' + isHidden}>
+        {list}
+      </ol>
+    );
   }
 }
