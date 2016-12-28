6e89b14d803f3e84f0214c1e885ebd6ae549c532
seafruit <18829290140@163.com>
2016-12-28 15:01:42 +0800

    modified ourwork and leukemia less

--- public/components/leukemia-assistance/StaticContent.js
+++ public/components/leukemia-assistance/StaticContent.js
@@ -28,8 +28,8 @@ export default class ContentStatic extends Component {
     return (
       <div>
         <div className='save-life-situation'>
-          <h2>我们参与挽救的生命</h2>
-          <div className='Rescue-situation-img'>
+          <h2 className="middle-title">我们参与挽救的生命</h2>
+          <div className='rescue-situation-img'>
             <img src='http://localhost/tw-ra/public/images/ourWorkAssistance/lives helped.png'/>
             <img src='http://localhost/tw-ra/public/images/ourWorkAssistance/raised.png'/>
           </div>
@@ -39,7 +39,7 @@ export default class ContentStatic extends Component {
         </div>
 
         <div className='fact-situation'>
-          <h2>你知道吗？</h2>
+          <h2 className="middle-title">你知道吗？</h2>
           <p>理论上，中国的儿童白血病的治愈率超过<span>70%</span>。但是只有<span>20%</span>的孩子能活下来。</p>
           <p>影响治愈率的是缺乏的<span>资金</span>。</p>
           <span> 你可以改变他们的命运。</span>
