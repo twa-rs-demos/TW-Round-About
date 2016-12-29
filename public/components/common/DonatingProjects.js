e5776415b57d4a4b254879384c592eb15e79ac9b
seafruit <18829290140@163.com>
2016-12-29 12:30:41 +0800

    modified donating projects

--- public/components/common/DonatingProjects.js
+++ public/components/common/DonatingProjects.js
@@ -5,18 +5,62 @@ import {Link} from 'react-router';
 export default class DonatingProjects extends Component {
   render() {
     const donatingProjectList = [
-      {img: 'http://localhost/tw-ra/public/images/donate/current appeals01.png', name: '程程', age: '8', disease: '白血病', money: '20,000'},
-      {img: 'http://localhost/tw-ra/public/images/donate/current appeals02.png', name: '程程', age: '8', disease: '白血病', money: '20,000'},
-      {img: 'http://localhost/tw-ra/public/images/donate/current appeals03.png', name: '程程', age: '8', disease: '白血病', money: '20,000'},
-      {img: 'http://localhost/tw-ra/public/images/donate/current appeals01.png', name: '程程', age: '8', disease: '白血病', money: '20,000'},
-      {img: 'http://localhost/tw-ra/public/images/donate/current appeals02.png', name: '程程', age: '8', disease: '白血病', money: '20,000'},
-      {img: 'http://localhost/tw-ra/public/images/donate/current appeals03.png', name: '程程', age: '8', disease: '白血病', money: '20,000'},
-      {img: 'http://localhost/tw-ra/public/images/donate/current appeals01.png', name: '程程', age: '8', disease: '白血病', money: '20,000'}
+      {
+        img: 'http://localhost/tw-ra/public/images/donate/current appeals01.png',
+        name: '程程',
+        age: '8',
+        disease: '白血病',
+        money: '20,000'
+      },
+      {
+        img: 'http://localhost/tw-ra/public/images/donate/current appeals02.png',
+        name: '程程',
+        age: '8',
+        disease: '白血病',
+        money: '20,000'
+      },
+      {
+        img: 'http://localhost/tw-ra/public/images/donate/current appeals03.png',
+        name: '程程',
+        age: '8',
+        disease: '白血病',
+        money: '20,000'
+      },
+      {
+        img: 'http://localhost/tw-ra/public/images/donate/current appeals01.png',
+        name: '程程',
+        age: '8',
+        disease: '白血病',
+        money: '20,000'
+      },
+      {
+        img: 'http://localhost/tw-ra/public/images/donate/current appeals02.png',
+        name: '程程',
+        age: '8',
+        disease: '白血病',
+        money: '20,000'
+      },
+      {
+        img: 'http://localhost/tw-ra/public/images/donate/current appeals03.png',
+        name: '程程',
+        age: '8',
+        disease: '白血病',
+        money: '20,000'
+      },
+      {
+        img: 'http://localhost/tw-ra/public/images/donate/current appeals01.png',
+        name: '程程',
+        age: '8',
+        disease: '白血病',
+        money: '20,000'
+      }
     ];
     return (
-      <div className='donating-projects donate-page-index'>
-        <h2 className='middle-title'>{this.props.title}</h2>
-        <div className='container'>
+      <div className='donating-projects'>
+        <div className="donating-middle-text">
+          <h2 className='middle-title'>{this.props.title}</h2>
+        </div>
+        <div>
           {this.getDonatingProjectsRow(donatingProjectList)}
         </div>
       </div>
@@ -41,12 +85,12 @@ export default class DonatingProjects extends Component {
           <div className='child-profile'>
             <p>这里是简介哦-。-！已经测试过长度的问题，不会越界的，放心哦^0^</p>
           </div>
-          <h4>{item.name}，{item.age}岁</h4>
-          <div>
+          <div className="child-profile-text">
+            <h4>{item.name}，{item.age}岁</h4>
             <p>{item.disease}</p>
             <p>需要&nbsp;￥{item.money}</p>
+            <Link to='/tw-ra/childStory'>阅读更多 ></Link>
           </div>
-          <Link to='/tw-ra/childStory'>阅读更多 ></Link>
         </div>
       );
     });
