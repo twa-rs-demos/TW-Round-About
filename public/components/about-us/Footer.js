2855f17f5f81ad6a0513aed2497eb8b85ac2cc51
MHwishes <1025383143@qq.com>
2017-01-09 19:00:13 +0800

    add axis

--- public/components/about-us/Footer.js
+++ public/components/about-us/Footer.js
@@ -1,13 +1,139 @@
 import {Component} from 'react';
 
 export default class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: false
+    };
+  }
+
+
+  showEvents2009() {
+    this.setState({
+      events: !this.state.events
+    });
+  }
 
   render() {
     return (
-      <div id='aboutUs-footer' className='row'>
-        <div className='col-xs-offset-1 col-xs-10'>
-          <h3 className='middle-title aboutUs-event'>众爱大事记</h3>
+      <div id='aboutUs-footer'>
+        <h3 className='middle-title aboutUs-event'>众爱大事记</h3>
+        <div className='container'>
+          <div className='row'>
+
+            <div className='col-xs-offset-1 col-xs-1 no-padding-right'>
+              <div className='time-line'></div>
+            </div>
+
+            <div className='col-xs-1 no-padding'>
+              <div className='time-line'>
+                <div className='circle-year'>
+                  <span className='year'>2008</span>
+                  <i className='fa fa-circle circle'> </i>
+                </div>
+              </div>
+            </div>
+
+            <div className='col-xs-1  no-padding'>
+              <div className='time-line'>
+                <div className='circle-year' onClick={this.showEvents2009.bind(this)}>
+                  <span className='year'>2009</span>
+                  <i className='fa fa-circle circle'> </i>
+                  <div className={this.state.events ? '' : 'hidden'}>
+                    <Events/>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className='col-xs-1 no-padding'>
+              <div className='time-line'>
+                <div className='circle-year'>
+                  <span className='year'>2010</span>
+                  <i className='fa fa-circle circle'> </i>
+                </div>
+              </div>
+            </div>
+
+            <div className='col-xs-1  no-padding'>
+              <div className='time-line'>
+                <div className='circle-year'>
+                  <span className='year'>2011</span>
+                  <i className='fa fa-circle circle'> </i>
+                </div>
+              </div>
+            </div>
+
+            <div className='col-xs-1  no-padding'>
+              <div className='time-line'>
+                <div className='circle-year'>
+                  <span className='year'>2012</span>
+                  <i className='fa fa-circle circle'> </i>
+                </div>
+              </div>
+            </div>
+
+            <div className='col-xs-1  no-padding'>
+              <div className='time-line'>
+                <div className='circle-year'>
+                  <span className='year'>2013</span>
+                  <i className='fa fa-circle circle'> </i>
+                </div>
+              </div>
+            </div>
+
+            <div className='col-xs-1  no-padding'>
+              <div className='time-line'>
+                <div className='circle-year'>
+                  <span className='year'>2014</span>
+                  <i className='fa fa-circle circle'> </i>
+                </div>
+              </div>
+            </div>
+
+            <div className='col-xs-1 time-axis no-padding'>
+              <div className='time-line'>
+                <div className='circle-year'>
+                  <span className='year'>2015</span>
+                  <i className='fa fa-circle circle'> </i>
+                </div>
+              </div>
+            </div>
+
+            <div className='col-xs-1 time-axis no-padding'>
+              <div className='time-line'>
+                <div className='circle-year'>
+                  <span className='year'>2016</span>
+                  <i className='fa fa-circle circle'> </i>
+                  <i className='fa fa-chevron-right circle chevron'> </i>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+
+      </div>
+    );
+  }
+}
+
+class Events extends Component {
+
+  render() {
+    return (
+      <div id='aboutUs-years-events'>
+
+        <div  className='fist-event'>
+          <div>FIRST BOOK FAIR</div>
+          <div>@ Mar 23,2009</div>
+        </div>
+
+        <div className='second-event'>
+          <div>FIRST BOOK FAIR</div>
+          <div>@ Mar 23,2009</div>
+        </div>
+
       </div>
     );
   }
