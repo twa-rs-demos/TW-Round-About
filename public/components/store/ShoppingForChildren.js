55c8d69a04deb19526ca5ccf1bbb825ef9682d2c
XLuoChen <1425145741@qq.com>
2017-01-09 11:28:54 +0800

    adjust responsive style

--- public/components/store/ShoppingForChildren.js
+++ public/components/store/ShoppingForChildren.js
@@ -7,16 +7,16 @@ export default class ShoppingForChildren extends Component {
       <p className='text-center'>所得利润用于支持和帮助困境群体。</p>
       <div className='col-xs-6 two-dimension-code no-padding'>
         <div className='col-xs-6 inline text-right no-padding relative-positon'>
-          <img src='http://localhost/tw-ra/public/images/wechat01.png' alt=''/>
+          <img className='store-picture' src='http://localhost/tw-ra/public/images/wechat01.png' alt=''/>
           <span className='store-name'>微信商城</span>
         </div>
         <div className='col-xs-6 inline text-right no-padding relative-positon'>
-          <img src='http://localhost/tw-ra/public/images/taobao01.png' alt=''/>
+          <img className='store-picture' src='http://localhost/tw-ra/public/images/taobao01.png' alt=''/>
           <span className='store-name'>淘宝商城</span>
         </div>
       </div>
       <div className='col-xs-6 donate-button'>
-        <div className='col-xs-10 text-right'>
+        <div className='col-xs-11 text-right'>
           <button>捐款</button>
           <button>捐物</button>
           <div>... Or if you’re in the giving mood</div>
