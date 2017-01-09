009298c9dc499c02d0218c273f3454a551050dde
XLuoChen <1425145741@qq.com>
2017-01-09 16:01:18 +0800

    sdjust style for getInvolved to be responsive

--- public/components/get-involved/index.js
+++ public/components/get-involved/index.js
@@ -7,9 +7,13 @@ export default class getInvolved extends Component {
   render() {
     const pictureSrc = 'http://localhost/tw-ra/public/images/getInvolved/hero_getInvolved.png';
     return (
-      <div id='get-involved'>
-        <MaxPicture src={pictureSrc}/>
-        <StaticContent/>
+      <div id='get-involved' className='row'>
+        <div>
+          <MaxPicture src={pictureSrc}/>
+        </div>
+        <div className='col-md-offset-2 col-md-8 col-sm-offset-1 col-sm-10 col-xs-offset-1 col-xs-10 no-padding'>
+          <StaticContent/>
+        </div>
       </div>
     );
   }
