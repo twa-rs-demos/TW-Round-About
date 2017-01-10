a8265363ec7d077a63230c686cd083e7ad568434
XLuoChen <1425145741@qq.com>
2017-01-10 16:33:22 +0800

    write publicResource

---
+++ public/components/classifieds/PublicResource.js
@@ -0,0 +1,13 @@
+import {Component} from 'react';
+
+export default class PublicResource extends Component{
+  render(){
+    return (<div>
+      <div className='col-sm-offset-2 col-sm-8 no-padding'>
+        <h2 className='middle-title'>共享资源</h2>
+        <p className='public-resource'>Roundabout is a social enterprise headed by volunteers. We provide a free service connecting those who wish
+          to give, be that monetary or otherwise, to those in need. Started in 2008, we </p>
+      </div>
+    </div>);
+  }
+}
--- public/components/classifieds/index.js
+++ public/components/classifieds/index.js
@@ -1,9 +1,11 @@
 import {Component} from 'react';
+import PublicResource from './PublicResource';
+import '../../style/classfields.less';
 
 export default class Classifieds extends Component {
   render() {
-    return (<div>
-      Classifieds
+    return (<div className='row' id='class-fields'>
+      <PublicResource/>
     </div>);
   }
 }
