6de9183d579a799b0bcc7ca147b776191046314e
MHwishes <1025383143@qq.com>
2017-01-10 18:16:03 +0800

    modified rem to px in axis

--- public/components/about-us/footer/Events.js
+++ public/components/about-us/footer/Events.js
@@ -4,15 +4,17 @@ export default class Events extends Component {
 
   render() {
     return (
-      <div id='aboutUs-years-events'>
-        <div className='fist-event'>
-          <div>{this.props.firstEvent}</div>
-          <div>{this.props.firstEventTime}</div>
-        </div>
+      <div className={this.props.firstEvent === '' ? 'hidden' : ''}>
+        <div id='aboutUs-years-events'>
+          <div className='fist-event'>
+            <div>{this.props.firstEvent}</div>
+            <div>{this.props.firstEventTime}</div>
+          </div>
 
-        <div className='second-event'>
-          <div>{this.props.secondEvent}</div>
-          <div>{this.props.secondEventTime}</div>
+          <div className='second-event'>
+            <div>{this.props.secondEvent}</div>
+            <div>{this.props.secondEventTime}</div>
+          </div>
         </div>
       </div>
     );
