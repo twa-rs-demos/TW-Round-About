2b3400221177d988695d366637f4981f408a51bf
MHwishes <1025383143@qq.com>
2017-01-10 10:35:35 +0800

    add axis style

--- public/components/about-us/CommonAxis.js
+++ public/components/about-us/CommonAxis.js
@@ -1,13 +1,19 @@
 import {Component} from 'react';
 
 export default class CommonAxis extends Component {
+  showEvents(){
+    this.props.everyYearEvents();
+  }
 
   render() {
     return (
-      <div>
-
-        
-
+      <div className='col-md-1 col-xs-1 no-padding'>
+        <div className='time-line'>
+          <div className='circle-year' onMouseOver={this.showEvents.bind(this)}>
+            <span className='year'>{this.props.year}</span>
+            <i className='fa fa-circle circle'> </i>
+          </div>
+        </div>
       </div>
     );
   }
--- public/components/about-us/Footer.js
+++ public/components/about-us/Footer.js
@@ -1,4 +1,5 @@
 import {Component} from 'react';
+import CommonAxis from './CommonAxis';
 
 export default class Footer extends Component {
   constructor(props) {
@@ -7,7 +8,6 @@ export default class Footer extends Component {
       events: false
     };
   }
-
 
   showEvents2009() {
     this.setState({
@@ -21,97 +21,24 @@ export default class Footer extends Component {
         <h3 className='middle-title aboutUs-event'>众爱大事记</h3>
         <div className='container aboutUs-container'>
           <div className='row'>
-
             <div className='col-md-offset-1 col-md-1  col-xs-offset-1 col-xs-1 no-padding-right'>
               <div className='time-line'></div>
             </div>
-
-            <div className='col-md-1 col-xs-1 no-padding'>
-              <div className='time-line'>
-                <div className='circle-year'>
-                  <span className='year'>2008</span>
-                  <i className='fa fa-circle circle'> </i>
-                </div>
-              </div>
-            </div>
-
-            <div className='col-md-1 col-xs-1 no-padding'>
-              <div className='time-line'>
-                <div className='circle-year' onClick={this.showEvents2009.bind(this)}>
-                  <span className='year'>2009</span>
-                  <i className='fa fa-circle circle'> </i>
-                </div>
-              </div>
-            </div>
-
-            <div className='col-md-1  col-xs-1 no-padding'>
-              <div className='time-line'>
-                <div className='circle-year'>
-                  <span className='year'>2010</span>
-                  <i className='fa fa-circle circle'> </i>
-                </div>
-              </div>
-            </div>
-
-            <div className='col-md-1 col-xs-1  no-padding'>
-              <div className='time-line'>
-                <div className='circle-year'>
-                  <span className='year'>2011</span>
-                  <i className='fa fa-circle circle'> </i>
-                </div>
-              </div>
-            </div>
-
-            <div className='col-md-1 col-xs-1 no-padding'>
-              <div className='time-line'>
-                <div className='circle-year'>
-                  <span className='year'>2012</span>
-                  <i className='fa fa-circle circle'> </i>
-                </div>
-              </div>
-            </div>
-
-            <div className='col-md-1 col-xs-1 no-padding'>
-              <div className='time-line'>
-                <div className='circle-year'>
-                  <span className='year'>2013</span>
-                  <i className='fa fa-circle circle'> </i>
-                </div>
-              </div>
-            </div>
-
-            <div className='col-md-1 col-xs-1 no-padding'>
-              <div className='time-line'>
-                <div className='circle-year'>
-                  <span className='year'>2014</span>
-                  <i className='fa fa-circle circle'> </i>
-                </div>
-              </div>
-            </div>
-
-            <div className='col-md-1 col-xs-1 no-padding'>
-              <div className='time-line'>
-                <div className='circle-year'>
-                  <span className='year'>2015</span>
-                  <i className='fa fa-circle circle'> </i>
-                </div>
-              </div>
-            </div>
-
-            <div className='col-md-1 col-xs-1 no-padding'>
-              <div className='time-line'>
-                <div className='circle-year'>
-                  <span className='year'>2016</span>
-                  <i className='fa fa-circle circle'> </i>
-                </div>
-              </div>
-            </div>
+            <CommonAxis year='2008'/>
+            <CommonAxis year='2009' everyYearEvents={this.showEvents2009.bind(this)}/>
+            <CommonAxis year='2010'/>
+            <CommonAxis year='2011'/>
+            <CommonAxis year='2012'/>
+            <CommonAxis year='2013'/>
+            <CommonAxis year='2014'/>
+            <CommonAxis year='2015'/>
+            <CommonAxis year='2016'/>
 
             <div className='col-md-1 col-xs-1 no-padding'>
               <i className='fa fa-chevron-right circle chevron'> </i>
             </div>
           </div>
-          <div className='col-xs-offset-3 col-xs-1'>
+          <div className='col-xs-offset-3 col-xs-1 no-padding'>
             <div className={this.state.events ? '' : 'hidden'}>
               <Events/>
             </div>
