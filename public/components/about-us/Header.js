bccaa7f7341efec8d0360afaf3feb5ef136efa39
MHwishes <1025383143@qq.com>
2017-01-10 12:03:45 +0800

    add button to donate page

--- public/components/about-us/Header.js
+++ public/components/about-us/Header.js
@@ -1,4 +1,5 @@
 import {Component} from 'react';
+import {Link} from 'react-router';
 
 export default class Header extends Component {
 
@@ -7,7 +8,9 @@ export default class Header extends Component {
       <div id='aboutUs-header'>
         <h3 className='middle-title aboutUs-title'>每个孩子都应该享有同样的机会</h3>
         <div className='text-center'>
-          <button className='button-style button-change-destiny'>改变TA的命运</button>
+          <Link to={URI_PREFIX + '/donate'}>
+            <button className='button-style button-change-destiny'>改变TA的命运</button>
+          </Link>
         </div>
         <div className='video about-video row'>
           <div className='col-md-offset-2 col-md-8 col-xs-12'>
@@ -15,7 +18,6 @@ export default class Header extends Component {
                    type='application/x-shockwave-flash' className='video-content'>
             </embed>
           </div>
-
         </div>
       </div>
     );
