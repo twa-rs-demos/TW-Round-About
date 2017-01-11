8e8ec82551d61bd65a5c1989b8366d903f5f240c
XLuoChen <1425145741@qq.com>
2017-01-11 19:15:03 +0800

    write filter modal for advanced search

--- public/components/classifieds/SearchBox.js
+++ public/components/classifieds/SearchBox.js
@@ -1,6 +1,22 @@
 import {Component} from 'react';
+import Filter from './Filter';
 
 export default class SearchBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShowFilter: false
+    };
+  }
+
+  showFilter() {
+    this.setState({isShowFilter: true});
+  }
+
+  hideFilter() {
+    this.setState({isShowFilter: false});
+  }
+
   render() {
     return (
       <div className='search-box row'>
@@ -10,7 +26,10 @@ export default class SearchBox extends Component {
             <img src='http://localhost/tw-ra/public/images/home/icon_search_small_focused.png' alt=''/>
           </button>
         </div>
-        <div className='col-sm-offset-4 col-xs-offset-3 col-sm-4 col-xs-6 no-padding search-type'>高级搜索 ></div>
+        <div className='col-sm-offset-4 col-xs-offset-3 col-sm-4 col-xs-6 no-padding search-type'>
+          <button onClick={this.showFilter.bind(this)}>高级搜索 ></button>
+        </div>
+        {this.state.isShowFilter ? <Filter hideFilter={this.hideFilter.bind(this)}/> : ''}
       </div>
     );
   }
