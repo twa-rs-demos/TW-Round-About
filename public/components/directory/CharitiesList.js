107626500d02fb8c700a6af67396843ef425c38e
XLuoChen <1425145741@qq.com>
2017-02-24 22:29:30 +0800

    extract Charity component

--- public/components/directory/CharitiesList.js
+++ public/components/directory/CharitiesList.js
@@ -1,66 +1,42 @@
 import React, {Component} from 'react';
+import Charity from './Charity';
+import Pagination from '../common/Pagination';
 
 export default class CharitiesList extends Component {
   render() {
+    const items = [{
+      name: 'AGS',
+      address: 'Yuyang Road West, Adjacent to Yosemite Villas, Houshayu Town, Shunyi District',
+      phoneNumber: '010-86753478',
+      description: 'Roundabout Book Nook AT YCIS Beijing Yew Chung International School of Beijing (YCIS Beijing) will hosta book nook on behalf of Roundabout. This ',
+      tags: ['Tag1', 'Tag2', 'Tag3']
+    }, {
+      name: 'AGS',
+      address: 'Yuyang Road West, Adjacent to Yosemite Villas, Houshayu Town, Shunyi District',
+      phoneNumber: '010-86753478',
+      description: 'Roundabout Book Nook AT YCIS Beijing Yew Chung International School of Beijing (YCIS Beijing) will hosta book nook on behalf of Roundabout. This ',
+      tags: ['Tag1', 'Tag2', 'Tag3']
+    }, {
+      name: 'AGS',
+      address: 'Yuyang Road West, Adjacent to Yosemite Villas, Houshayu Town, Shunyi District',
+      phoneNumber: '010-86753478',
+      description: 'Roundabout Book Nook AT YCIS Beijing Yew Chung International School of Beijing (YCIS Beijing) will hosta book nook on behalf of Roundabout. This ',
+      tags: ['Tag1', 'Tag2', 'Tag3']
+    }, {
+      name: 'AGS',
+      address: 'Yuyang Road West, Adjacent to Yosemite Villas, Houshayu Town, Shunyi District',
+      phoneNumber: '010-86753478',
+      description: 'Roundabout Book Nook AT YCIS Beijing Yew Chung International School of Beijing (YCIS Beijing) will hosta book nook on behalf of Roundabout. This ',
+      tags: ['Tag1', 'Tag2', 'Tag3']
+    }];
+
+    const charities = items.map((item, index) => {
+      return <Charity charity={item} key={index}/>;
+    });
+
     return (<div>
-      <div className='row charities'>
-        <div className='col-xs-2 no-padding'>
-          <img className='image'
-               src='http://localhost/tw-ra/public/images/partners/logo_AGS.png' alt=''/>
-        </div>
-        <div className='col-xs-10 no-padding'>
-          <h5 className='charity-name'>AGS</h5>
-          <div className='charity-info'>
-            <div>
-              <i className='fa fa-home' aria-hidden='true'></i>
-              <span
-                className='span'>Yuyang Road West, Adjacent to Yosemite Villas, Houshayu Town, Shunyi District</span>
-            </div>
-            <div>
-              <i className='fa fa-phone' aria-hidden='true'></i>
-              <span className='span'>010-86753478</span>
-            </div>
-            <div>
-              <span className='span'>Roundabout Book Nook AT YCIS Beijing Yew Chung International School of Beijing (YCIS Beijing) will host
-                a book nook on behalf of Roundabout. This </span>
-            </div>
-            <div>
-              <span className='tag'>Tag1</span>
-              <span className='tag'>Tag2</span>
-              <span className='tag'>Tag3</span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className='row charities'>
-        <div className='col-xs-2 no-padding'>
-          <img className='image'
-               src='http://localhost/tw-ra/public/images/partners/logo_AGS.png' alt=''/>
-        </div>
-        <div className='col-xs-10 no-padding'>
-          <h5 className='charity-name'>AGS</h5>
-          <div className='charity-info'>
-            <div>
-              <i className='fa fa-home' aria-hidden='true'></i>
-              <span
-                className='span'>Yuyang Road West, Adjacent to Yosemite Villas, Houshayu Town, Shunyi District</span>
-            </div>
-            <div>
-              <i className='fa fa-phone' aria-hidden='true'></i>
-              <span className='span'>010-86753478</span>
-            </div>
-            <div>
-              <span className='span'>Roundabout Book Nook AT YCIS Beijing Yew Chung International School of Beijing (YCIS Beijing) will host
-                a book nook on behalf of Roundabout. This </span>
-            </div>
-            <div>
-              <span className='tag'>Tag1</span>
-              <span className='tag'>Tag2</span>
-              <span className='tag'>Tag3</span>
-            </div>
-          </div>
-        </div>
-      </div>
+      {charities}
+      <Pagination totalPage='3'/>
     </div>);
   }
 }
