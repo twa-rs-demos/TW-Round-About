788e6463bdcb903de6f22363fa87d4efaf3fbf22
XLuoChen <1425145741@qq.com>
2017-02-24 22:01:03 +0800

    fix eslint

--- public/components/directory/CharitiesList.js
+++ public/components/directory/CharitiesList.js
@@ -3,60 +3,60 @@ import React, {Component} from 'react';
 export default class CharitiesList extends Component {
   render() {
     return (<div>
-      <div className="row charities">
-        <div className="col-xs-2 no-padding">
+      <div className='row charities'>
+        <div className='col-xs-2 no-padding'>
           <img className='image'
                src='http://localhost/tw-ra/public/images/partners/logo_AGS.png' alt=''/>
         </div>
-        <div className="col-xs-10 no-padding">
-          <h5 className="charity-name">AGS</h5>
-          <div className="charity-info">
+        <div className='col-xs-10 no-padding'>
+          <h5 className='charity-name'>AGS</h5>
+          <div className='charity-info'>
             <div>
-              <i className="fa fa-home" aria-hidden="true"></i>
+              <i className='fa fa-home' aria-hidden='true'></i>
               <span
-                className="span">Yuyang Road West, Adjacent to Yosemite Villas, Houshayu Town, Shunyi District</span>
+                className='span'>Yuyang Road West, Adjacent to Yosemite Villas, Houshayu Town, Shunyi District</span>
             </div>
             <div>
-              <i className="fa fa-phone" aria-hidden="true"></i>
-              <span className="span">010-86753478</span>
+              <i className='fa fa-phone' aria-hidden='true'></i>
+              <span className='span'>010-86753478</span>
             </div>
             <div>
-              <span className="span">Roundabout Book Nook AT YCIS Beijing Yew Chung International School of Beijing (YCIS Beijing) will host
+              <span className='span'>Roundabout Book Nook AT YCIS Beijing Yew Chung International School of Beijing (YCIS Beijing) will host
                 a book nook on behalf of Roundabout. This </span>
             </div>
             <div>
-              <span className="tag">Tag1</span>
-              <span className="tag">Tag2</span>
-              <span className="tag">Tag3</span>
+              <span className='tag'>Tag1</span>
+              <span className='tag'>Tag2</span>
+              <span className='tag'>Tag3</span>
             </div>
           </div>
         </div>
       </div>
-      <div className="row charities">
-        <div className="col-xs-2 no-padding">
+      <div className='row charities'>
+        <div className='col-xs-2 no-padding'>
           <img className='image'
                src='http://localhost/tw-ra/public/images/partners/logo_AGS.png' alt=''/>
         </div>
-        <div className="col-xs-10 no-padding">
-          <h5 className="charity-name">AGS</h5>
-          <div className="charity-info">
+        <div className='col-xs-10 no-padding'>
+          <h5 className='charity-name'>AGS</h5>
+          <div className='charity-info'>
             <div>
-              <i className="fa fa-home" aria-hidden="true"></i>
+              <i className='fa fa-home' aria-hidden='true'></i>
               <span
-                className="span">Yuyang Road West, Adjacent to Yosemite Villas, Houshayu Town, Shunyi District</span>
+                className='span'>Yuyang Road West, Adjacent to Yosemite Villas, Houshayu Town, Shunyi District</span>
             </div>
             <div>
-              <i className="fa fa-phone" aria-hidden="true"></i>
-              <span className="span">010-86753478</span>
+              <i className='fa fa-phone' aria-hidden='true'></i>
+              <span className='span'>010-86753478</span>
             </div>
             <div>
-              <span className="span">Roundabout Book Nook AT YCIS Beijing Yew Chung International School of Beijing (YCIS Beijing) will host
+              <span className='span'>Roundabout Book Nook AT YCIS Beijing Yew Chung International School of Beijing (YCIS Beijing) will host
                 a book nook on behalf of Roundabout. This </span>
             </div>
             <div>
-              <span className="tag">Tag1</span>
-              <span className="tag">Tag2</span>
-              <span className="tag">Tag3</span>
+              <span className='tag'>Tag1</span>
+              <span className='tag'>Tag2</span>
+              <span className='tag'>Tag3</span>
             </div>
           </div>
         </div>
--- public/components/directory/index.js
+++ public/components/directory/index.js
@@ -4,14 +4,14 @@ import CharitiesList from './CharitiesList';
 
 export default class Directory extends Component {
   render() {
-    return (<div id="directory">
-        <div className="row">
-          <div className="col-sm-offset-2 col-sm-8 no-padding advanced-search">
+    return (<div id='directory'>
+        <div className='row'>
+          <div className='col-sm-offset-2 col-sm-8 no-padding advanced-search'>
             <SearchBox/>
           </div>
         </div>
-        <div className="row">
-          <div className="col-sm-offset-2 col-sm-8 no-padding charities-list">
+        <div className='row'>
+          <div className='col-sm-offset-2 col-sm-8 no-padding charities-list'>
             <CharitiesList/>
           </div>
         </div>
