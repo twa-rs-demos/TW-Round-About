e42bfbd2b8d41ab6e97772edf4d079a44bdab6bc
XLuoChen <1425145741@qq.com>
2017-02-27 16:55:39 +0800

    add mask for menu when scree reduce

--- public/components/layout/header/NavMenu.js
+++ public/components/layout/header/NavMenu.js
@@ -5,12 +5,28 @@ import SearchBox from './SearchBox';
 export default class NavMenu extends Component {
   render() {
     return (
-      <div className='row no-margin'>
-        <div className='col-md-10 col-xs-7'>
-          <MenuList/>
+      <div>
+        <div className='row no-margin menu-list1'>
+          <div className='col-md-10 col-xs-7'>
+            <MenuList/>
+          </div>
+          <div className='col-md-2 col-xs-5 text-right no-padding header-search-box'>
+            <SearchBox/>
+          </div>
         </div>
-        <div className='col-md-2 col-xs-5 text-right no-padding search-box'>
-          <SearchBox/>
+        <a href="#" className="menu-icon">==</a>
+        <div className="menu-list2">
+          {/*<div className="overlay"></div>*/}
+
+          <div className="sidebar-wrapper"></div>
+          <div className="nav-bar">
+            <ul className="sidebar-nav">
+              <li>f</li>
+              <li>f</li>
+              <li>f</li>
+              <li>f</li>
+            </ul>
+          </div>
         </div>
       </div>
     );
