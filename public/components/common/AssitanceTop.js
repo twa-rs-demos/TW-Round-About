45e773a9f773057317c78face42d0287603e1755
seafruit <18829290140@163.com>
2017-02-28 10:02:36 +0800

    assistance 3 pages include medical,education and disaster

--- public/components/common/AssitanceTop.js
+++ public/components/common/AssitanceTop.js
@@ -3,12 +3,15 @@ import React, {Component} from 'react';
 export default class MedicalAssitanceTop extends Component {
   render() {
     return (
-      <div className="medical-assistance-top">
+      <div className="assistance-top">
         <div>
-          <h2 className="middle-title">医疗救助</h2>
+          <h2 className="middle-title">{this.props.title}</h2>
         </div>
-        <div className="">
-          <img src="#"/>
+        <div className="assistence-pic-container">
+          <img src={this.props.pictureSrc}/>
+        </div>
+        <div className="col-sm-offset-2 col-sm-8 col-xs-offset-1 col-xs-10 assistance-describe">
+          <p className="css-wrap">{this.props.describe}</p>
         </div>
       </div>
     );
