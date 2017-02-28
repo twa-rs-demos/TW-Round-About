b70051df170e1a17ce9cd9c9634a1fcef08bda30
seafruit <18829290140@163.com>
2017-02-28 10:26:12 +0800

    newsletter page

--- public/components/common/AssitanceTop.js
+++ public/components/common/AssitanceTop.js
@@ -1,7 +1,21 @@
 import React, {Component} from 'react';
 
 export default class MedicalAssitanceTop extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  componentDidMount() {
+    console.log(this.props.describe);
+    console.log(this.refs.describe.className);
+    if(this.props.describe ===''){
+      this.refs.describe.className='';
+    }
+
+  }
+
   render() {
+    console.log(this.props.describe);
     return (
       <div className="assistance-top">
         <div>
@@ -11,7 +25,7 @@ export default class MedicalAssitanceTop extends Component {
           <img src={this.props.pictureSrc}/>
         </div>
         <div className="col-sm-offset-2 col-sm-8 col-xs-offset-1 col-xs-10 assistance-describe">
-          <p className="css-wrap">{this.props.describe}</p>
+          <p className="css-wrap describe-p" ref='describe'>{this.props.describe}</p>
         </div>
       </div>
     );
