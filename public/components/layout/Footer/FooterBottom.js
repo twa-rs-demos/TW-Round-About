d7601bb1c9b9ed95617a0876013aea4e091d1b33
seafruit <18829290140@163.com>
2017-03-06 09:35:25 +0800

    adjust footer compoment

--- public/components/layout/Footer/FooterBottom.js
+++ public/components/layout/Footer/FooterBottom.js
@@ -5,7 +5,7 @@ import '../../../style/legalPrivacyPolicy.less';
 export default class FooterBottom extends Component {
   render() {
     return (
-      <div className='col-xs-offset-6'>
+      <div className='col-sm-offset-6 col-sm-6 col-xs-12'>
         <div className='footer-bottom-text'>
           @2016 Roundabout China
           <Link to={URI_PREFIX + '/legal'} className='footBottom-info'>&nbsp; Legal</Link> |
--- public/components/layout/Footer/FooterTop.js
+++ public/components/layout/Footer/FooterTop.js
@@ -8,16 +8,16 @@ export default class FooterTop extends Component {
   render() {
     return (
       <div className='row'>
-        <div className='col-sm-3 col-xs-12 no-padding footer-item'>
+        <div className='col-md-3 col-sm-6 col-xs-12 no-padding footer-item'>
           <FooterTopOne/>
         </div>
-        <div className='col-sm-3 col-xs-12 no-padding footer-item'>
+        <div className='col-md-3 col-sm-6 col-xs-12 no-padding footer-item'>
           <FooterTopTwo/>
         </div>
-        <div className='col-sm-2 col-xs-12 no-padding footer-item'>
+        <div className='col-md-2 col-sm-7 col-xs-12 no-padding footer-item'>
           <FooterTopThree/>
         </div>
-        <div className='col-sm-4 col-xs-12 no-padding footer-item'>
+        <div className='col-md-4 col-sm-12 col-xs-12 no-padding footer-item'>
           <FooterTopFour/>
         </div>
       </div>
