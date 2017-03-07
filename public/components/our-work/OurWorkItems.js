0988680e8a305ce9e65a2d550503f068bc155ba3
MHwishes <1025383143@qq.com>
2017-03-07 10:05:29 +0800

    fix ourwork images

--- public/components/our-work/OurWorkItems.js
+++ public/components/our-work/OurWorkItems.js
@@ -1,5 +1,6 @@
 import {Component} from 'react';
 import {Link} from 'react-router';
+import images from './images';
 
 export default class ourWorkItems extends Component {
   render() {
@@ -12,19 +13,19 @@ export default class ourWorkItems extends Component {
           <div className='job-icons row icons-margin-bottom'>
             <div className='col-md-4 col-xs-12 icon-container'>
               <div className='job-icons-container'>
-                <img src='http://localhost/tw-ra/public/images/ourWork/icon_leukemia assistance_bg.png' alt='白血病救助'/>
+                <img src={images.iconLeukemia} alt='白血病救助'/>
               </div>
               <Link to={URI_PREFIX + '/ourwork/leukemiaassistance'}>白血病救助</Link>
             </div>
             <div className='col-md-4 col-xs-12 icon-container'>
               <div className='job-icons-container'>
-                <img src='http://localhost/tw-ra/public/images/ourWork/icon_disaster relief_bg.png' alt='医疗救助'/>
+                <img src={images.iconDisaster} alt='医疗救助'/>
               </div>
               <Link to={URI_PREFIX + '/ourwork/medicalassistance'}>医疗救助</Link>
             </div>
             <div className='col-md-4 col-xs-12 icon-container'>
               <div className='job-icons-container'>
-                <img src='http://localhost/tw-ra/public/images/ourWork/icon_education support_bg.png' alt='教育资助'/>
+                <img src={images.iconEducation} alt='教育资助'/>
               </div>
               <Link to={URI_PREFIX + '/ourwork/educationsupport'}>教育资助</Link>
             </div>
@@ -32,13 +33,13 @@ export default class ourWorkItems extends Component {
           <div className='job-icons row icons-margin-bottom'>
             <div className='col-md-6 col-xs-12 col-md-offset-1 icon-container'>
               <div className='job-icons-container'>
-                <img src='http://localhost/tw-ra/public/images/ourWork/icon_medical assistance_bg.png' alt='灾区救助'/>
+                <img src={images.iconMedical} alt='灾区救助'/>
               </div>
               <Link to={URI_PREFIX + '/ourwork/disasterrelief'}>灾区救助</Link>
             </div>
             <div className='col-md-2 col-xs-12 icon-container'>
               <div className='job-icons-container'>
-                <img src='http://localhost/tw-ra/public/images/ourWork/icon_community assistance_bg.png' alt='社区帮扶'/>
+                <img src={images.icon_Community} alt='社区帮扶'/>
               </div>
               <Link to={URI_PREFIX + '/ourwork/communityassistance'}>社区帮扶</Link>
             </div>
---
+++ public/components/our-work/images.js
@@ -0,0 +1,12 @@
+import iconLeukemia from '../../images/ourWork/icon_leukemia assistance_bg.png';
+import  iconDisaster from  '../../images/ourWork/icon_disaster relief_bg.png';
+import iconEducation from '../../images/ourWork/icon_education support_bg.png';
+import iconMedical from '../../images/ourWork/icon_medical assistance_bg.png';
+import icon_Community from '../../images/ourWork/icon_community assistance_bg.png';
+module.exports = {
+  iconLeukemia,
+  iconDisaster,
+  iconEducation,
+  iconMedical,
+  icon_Community
+};
